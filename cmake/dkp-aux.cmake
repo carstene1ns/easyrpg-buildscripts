41a060f8855928b30ebb991e1197c8e17bcd66f1
Carsten Teibes <dev@f4ke.de>
2022-11-02 00:42:42 +0100

    CMake: Add switch support - Fallback for downloads on easyrpg.org

--- cmake/dkp-aux.cmake
+++ cmake/dkp-aux.cmake
@@ -6,13 +6,23 @@ function(check_dkp_env platform) # BASE, 3DS, SWITCH, WII
 		endif()
 	elseif(platform STREQUAL 3DS)
 		if(NOT DEFINED ENV{DEVKITARM})
-			message(WARNING "Did not find $DEVKITARM environment variable!")
+			message(FATAL_ERROR "Did not find $DEVKITARM environment variable!")
+		endif()
+		if(NOT EXISTS $ENV{DEVKITARM})
+			message(FATAL_ERROR "Did not find devkitARM!")
 		endif()
 
 		set(libpath $ENV{DEVKITPRO}/libctru/lib)
 		find_library(LIB_CTRU ctru PATHS ${libpath} REQUIRED)
 		find_library(LIB_C3D citro3d PATHS ${libpath} REQUIRED)
 		find_library(LIB_C2D citro2d PATHS ${libpath} REQUIRED)
+		# tools are checked by toolchain file
+	elseif(platform STREQUAL SWITCH)
+		if(NOT EXISTS $ENV{DEVKITPRO}/devkitA64)
+			message(FATAL_ERROR "Did not find devkitA64!")
+		endif()
+
+		find_library(LIB_NX nx PATHS $ENV{DEVKITPRO}/libnx/lib REQUIRED)
 		# tools are checked by toolchain file
 	else()
 		message(FATAL_ERROR "Unknown DKP platform!")
--- cmake/functions.cmake
+++ cmake/functions.cmake
@@ -48,9 +48,21 @@ function(run_with_toolchain platform toolchain_file)
 			-DENABLE_CCACHE=${ENABLE_CCACHE}
 			-DENABLE_LIBLCF=${ENABLE_LCF}
 			-DBUILD_LIB_VERBOSE=${BUILD_LIB_VERBOSE}
-			-DCMAKE_VERBOSE_MAKEFILE=ON
+		#	-DCMAKE_VERBOSE_MAKEFILE=ON
 		INSTALL_COMMAND ${CMAKE_COMMAND} -E echo "Target Toolchain is at \"${CMAKE_CURRENT_SOURCE_DIR}\"."
+		STEP_TARGETS configure build
 	)
+
+	# add convenience targets
+	parse_libs(${CMAKE_CURRENT_SOURCE_DIR}/CMakeLists.txt LIB_TARGETS)
+	#message(NOTICE "Adding convenience targets for: ${LIB_TARGETS}.")
+	foreach(lib ${LIB_TARGETS})
+		add_custom_target(cross-${lib} ${CMAKE_COMMAND} --build . --target ${name}-configure
+			COMMAND ${CMAKE_COMMAND} --build ${name} --target ${lib}
+			COMMENT "Building ${lib} in cross environment..."
+			VERBATIM
+		)
+	endforeach()
 endfunction()
 
 ####################
@@ -60,10 +72,10 @@ endfunction()
 function(build_lib name)
 	cmake_parse_arguments(PARSE_ARGV 1 arg "CMAKE;AUTOTOOLS;VERBOSE;DEBUG" "SUBDIR" "PREFIX;OPTIONS;PATCHES")
 	if(DEFINED arg_UNPARSED_ARGUMENTS)
-		message(WARNING "build_lib: passed extra arguments: ${arg_UNPARSED_ARGUMENTS}")
+		message(WARNING "build_lib: Passed extra arguments: ${arg_UNPARSED_ARGUMENTS}")
 	endif()
 	if(DEFINED arg_KEYWORDS_MISSING_VALUES)
-		message(WARNING "build_lib: missing arguments for: ${arg_KEYWORDS_MISSING_VALUES}")
+		message(WARNING "build_lib: Missing arguments for: ${arg_KEYWORDS_MISSING_VALUES}")
 	endif()
 
 	# external debugging
@@ -74,10 +86,18 @@ function(build_lib name)
 	# download type
 	if(${arg_PREFIX}_URL)
 		list(APPEND DOWNLOAD_OPTIONS URL ${${arg_PREFIX}_URL})
+		# add our mirror
+		string(REGEX REPLACE ".*/(.*)$" "\\1" file ${${arg_PREFIX}_URL})
+		if(file)
+			list(APPEND DOWNLOAD_OPTIONS "https://easyrpg.org/downloads/sources/${file}")
+		else()
+			message(WARNING "build_lib: Could not extract file name (${file}) from URL: ${${arg_PREFIX}_URL}, not using mirror")
+		endif()
+
 		if(${arg_PREFIX}_SHA256)
 			list(APPEND DOWNLOAD_OPTIONS URL_HASH SHA256=${${arg_PREFIX}_SHA256})
 		else()
-			message(WARNING "No hash for: ${name}, redownloading always")
+			message(WARNING "build_lib: No hash for: ${name}, redownloading always")
 		endif()
 		list(APPEND DOWNLOAD_OPTIONS DOWNLOAD_NO_PROGRESS TRUE)
 	elseif(${arg_PREFIX}_GITURL)
@@ -85,7 +105,7 @@ function(build_lib name)
 		if(${arg_PREFIX}_GITVER)
 			list(APPEND DOWNLOAD_OPTIONS GIT_TAG ${${arg_PREFIX}_GITVER})
 		else()
-			message(WARNING "No git version for: ${name}, using master")
+			message(WARNING "build_lib: No git version for: ${name}, using master")
 		endif()
 	endif()
 
@@ -96,67 +116,63 @@ function(build_lib name)
 	set(PATCHES ${${arg_PREFIX}_PATCHES} ${arg_PATCHES})
 	list(LENGTH PATCHES NUM_PATCHES)
 	if(NUM_PATCHES GREATER 0)
-		set(PATCH_OPTION PATCH)
+		#set(PATCH_OPTION PATCH)
 	endif()
 
 	# additional options
-	# this is only needed for libmpg123 currently
 	if(arg_SUBDIR)
+		# this is only needed for libmpg123 currently
 		list(APPEND ADDITIONAL_OPTIONS SOURCE_SUBDIR ${arg_SUBDIR})
 	endif()
 
 	# directory setup
 	set_directory_properties(PROPERTIES EP_BASE libraries)
-	list(APPEND ADDITIONAL_OPTIONS INSTALL_DIR ${CMAKE_CURRENT_SOURCE_DIR} LOG_DIR log)
+	list(APPEND ADDITIONAL_OPTIONS
+		INSTALL_DIR ${CMAKE_CURRENT_SOURCE_DIR}
+		LOG_DIR log
+		DOWNLOAD_DIR download
+	)
+
 	if(NOT arg_VERBOSE)
-		# enable logging instead of terminal
-		list(APPEND ADDITIONAL_OPTIONS LOG_DOWNLOAD TRUE LOG_UPDATE TRUE LOG_PATCH TRUE LOG_CONFIGURE TRUE LOG_BUILD TRUE LOG_INSTALL TRUE LOG_MERGED_STDOUTERR TRUE LOG_OUTPUT_ON_FAILURE TRUE)
+		# enable file logging instead of terminal
+		list(APPEND ADDITIONAL_OPTIONS
+			LOG_DOWNLOAD TRUE
+			LOG_UPDATE TRUE
+			LOG_PATCH TRUE
+			LOG_CONFIGURE TRUE
+			LOG_BUILD TRUE
+			LOG_INSTALL TRUE
+			LOG_MERGED_STDOUTERR TRUE
+			LOG_OUTPUT_ON_FAILURE TRUE)
 		list(APPEND GENERATOR_OPTIONS -DCMAKE_COLOR_MAKEFILE=OFF)
 	endif()
 
-	if(arg_CMAKE)
-		if(ENABLE_CCACHE)
-			list(PREPEND GENERATOR_OPTIONS -DCMAKE_CXX_COMPILER_LAUNCHER=${CMAKE_CXX_COMPILER_LAUNCHER} -DCMAKE_C_COMPILER_LAUNCHER=${CMAKE_C_COMPILER_LAUNCHER})
-		endif()
-
-		# needed for all cross builds besides emscripten
-		if(CMAKE_TOOLCHAIN_FILE)
-			list(PREPEND GENERATOR_OPTIONS -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE})
-		endif()
-
-	elseif(arg_AUTOTOOLS)
-		message(WARNING "build_lib: autotools not implemented.")
-	else()
-		message(FATAL_ERROR "build_lib: build type not implemented.")
-	endif()
-
-	# debug
 	if(arg_DEBUG)
-		message(NOTICE "Config for ${name}:")
-		list(JOIN DOWNLOAD_OPTIONS "\n\t" _DOWNLOAD_OPTIONS)
-		message(NOTICE "[Download Options]\n${_DOWNLOAD_OPTIONS}")
-		list(JOIN PATCH_OPTIONS "\n\t" _PATCH_OPTIONS)
-		message(NOTICE "[Patch Options]\n${_PATCH_OPTIONS}")
-		list(JOIN GENERATOR_OPTIONS "\n" _GENERATOR_OPTIONS)
-		message(NOTICE "[Generator Options]\n${_GENERATOR_OPTIONS}")
-		list(JOIN ADDITIONAL_OPTIONS "\n\t" _ADDITIONAL_OPTIONS)
-		message(NOTICE "[Additional Options]\n${_ADDITIONAL_OPTIONS}")
-		message(NOTICE "=========================================")
+		# generate immediate targets
+		list(APPEND ADDITIONAL_OPTIONS STEP_TARGETS download update patch configure build install)
 	endif()
 
 	if(arg_CMAKE)
-		build_lib_cmake(${name}
+		build_lib_cmake(${name} DEBUG ${arg_DEBUG}
 			DOWNLOAD ${DOWNLOAD_OPTIONS}
 			${PATCH_OPTION} ${PATCH_OPTIONS}
 			GENERATOR ${GENERATOR_OPTIONS}
 			ADDITIONAL ${ADDITIONAL_OPTIONS}
 		)
 	elseif(arg_AUTOTOOLS)
-		# TODO
+		build_lib_autotools(${name} DEBUG ${arg_DEBUG}
+			DOWNLOAD ${DOWNLOAD_OPTIONS}
+			${PATCH_OPTION} ${PATCH_OPTIONS}
+			GENERATOR ${GENERATOR_OPTIONS}
+			ADDITIONAL ${ADDITIONAL_OPTIONS}
+		)
+	else()
+		message(FATAL_ERROR "build_lib: Build type not implemented.")
 	endif()
+
 endfunction()
 
-# 
+# remove unneeded toolchain dirs after building
 
 function(cleanup subdir)
 	add_custom_command(TARGET ${subdir}-cross POST_BUILD
@@ -167,10 +183,24 @@ function(cleanup subdir)
 	)
 endfunction()
 
-# internal functions/macros
+# Parses a list of library targets to be consumed by parent build
+
+function(parse_libs file var)
+	set(regex "^[^\#]+build_lib ?\\(([^ ]+).*$")
+	file(STRINGS ${file} build_libs REGEX ${regex})
+	foreach(l ${build_libs})
+		string(REGEX REPLACE ${regex} "\\1" lib ${l})
+		list(APPEND libs ${lib})
+	endforeach()
+	set(${var} ${libs} PARENT_SCOPE)
+endfunction()
+
+####################
+
+# helper functions/macros
 
 macro(build_lib_setup_args)
-	cmake_parse_arguments(PARSE_ARGV 1 "arg" "" "GENERATOR_WRAPPER" "DOWNLOAD;PATCH;GENERATOR;ADDITIONAL")
+	cmake_parse_arguments(PARSE_ARGV 1 "arg" "" "DEBUG;GENERATOR_WRAPPER" "DOWNLOAD;PATCH;GENERATOR;ADDITIONAL")
 	if(DEFINED arg_UNPARSED_ARGUMENTS)
 		message(WARNING "build_lib_setup_args: passed extra arguments: ${arg_UNPARSED_ARGUMENTS}")
 	endif()
@@ -179,23 +209,56 @@ macro(build_lib_setup_args)
 	endif()
 endmacro()
 
+macro(build_lib_debug)
+	if(arg_DEBUG)
+		message(NOTICE "=== Config for ${name} ===")
+		list(JOIN arg_DOWNLOAD "\n\t" _DOWNLOAD)
+		message(NOTICE "Download Options:\n\t${_DOWNLOAD}")
+		list(JOIN arg_PATCH "\n\t" _PATCH)
+		message(NOTICE "Patch Options:\n\t${_PATCH}")
+		list(JOIN arg_GENERATOR "\n\t" _GENERATOR)
+		message(NOTICE "Generator Options:\n\t${_GENERATOR}")
+		list(JOIN arg_ADDITIONAL "\n\t" _ADDITIONAL)
+		message(NOTICE "Additional Options:\n\t${_ADDITIONAL}")
+		message(NOTICE "")
+	endif()
+endmacro()
+
 function(build_lib_cmake name)
 	build_lib_setup_args()
+
+	if(ENABLE_CCACHE)
+		list(PREPEND arg_GENERATOR
+			-DCMAKE_CXX_COMPILER_LAUNCHER=${CMAKE_CXX_COMPILER_LAUNCHER}
+			-DCMAKE_C_COMPILER_LAUNCHER=${CMAKE_C_COMPILER_LAUNCHER}
+		)
+	endif()
+
+	# needed for all cross builds besides emscripten
+	if(CMAKE_TOOLCHAIN_FILE)
+		list(PREPEND arg_GENERATOR -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE})
+	endif()
+
+	# common flags
+	list(PREPEND arg_GENERATOR -DBUILD_SHARED_LIBS=OFF
+		-DCMAKE_BUILD_TYPE=RelWithDebInfo
+		-DCMAKE_INSTALL_PREFIX=${CMAKE_CURRENT_SOURCE_DIR}
+		#-DCMAKE_VERBOSE_MAKEFILE=ON
+	)
+
+	build_lib_debug()
 
 	ExternalProject_Add(${name}
 		${arg_DOWNLOAD}
 		${arg_PATCH}
-		CMAKE_ARGS
-			-DBUILD_SHARED_LIBS=OFF
-			-DCMAKE_BUILD_TYPE=RelWithDebInfo
-			-DCMAKE_INSTALL_PREFIX=${CMAKE_CURRENT_SOURCE_DIR}
-			-DCMAKE_VERBOSE_MAKEFILE=ON
-			${arg_GENERATOR}
+		CMAKE_ARGS ${arg_GENERATOR}
 		${arg_ADDITIONAL}
 	)
 endfunction()
 
 function(build_lib_autotools name)
 	build_lib_setup_args()
+
+	build_lib_debug()
 
 endfunction()
